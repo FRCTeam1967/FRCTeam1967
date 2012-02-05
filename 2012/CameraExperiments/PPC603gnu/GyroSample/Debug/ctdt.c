843368472f65f7f3a1bceaa6482fd3c477357b07
Janksters Dev-C <jankster.dev.c@gmail.com>
2012-02-05 10:39:25 -0800

    First attempt to checkin the experimental camera code.

---
+++ 2012/CameraExperiments/PPC603gnu/GyroSample/Debug/ctdt.c
@@ -0,0 +1,74 @@
+/* ctors and dtors arrays -- to be used by runtime system */
+/*   to call static constructors and destructors          */
+/*                                                        */
+/* NOTE: Use a C compiler to compile this file. If you    */
+/*       are using GNU C++, be sure to use compile this   */
+/*       file using a GNU compiler with the               */
+/*       -fdollars-in-identifiers flag.                   */
+
+
+void _GLOBAL__I__Z20FRC_userClassFactoryv();
+
+void _GLOBAL__I__ZN12PrintCommandC2EPKc();
+
+void _GLOBAL__I__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__I__ZN11WaitCommandC2Ed();
+
+void _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__I__ZN13NetworkTables3Key11_staticLockE();
+
+void _GLOBAL__I__ZN12NetworkTable13_tableNameMapE();
+
+void _GLOBAL__I__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__I__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__I_AxisCamera_debugFlag();
+
+extern void (*_ctors[])();
+void (*_ctors[])() =
+    {
+    _GLOBAL__I__Z20FRC_userClassFactoryv,
+    _GLOBAL__I__ZN12PrintCommandC2EPKc,
+    _GLOBAL__I__ZN9Scheduler9_instanceE,
+    _GLOBAL__I__ZN11WaitCommandC2Ed,
+    _GLOBAL__I_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__I__ZN13NetworkTables3Key11_staticLockE,
+    _GLOBAL__I__ZN12NetworkTable13_tableNameMapE,
+    _GLOBAL__I__ZN9RobotBase10m_instanceE,
+    _GLOBAL__I__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__I_AxisCamera_debugFlag,
+    0
+    };
+
+void _GLOBAL__D__Z20FRC_userClassFactoryv();
+
+void _GLOBAL__D__ZN9Scheduler9_instanceE();
+
+void _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange();
+
+void _GLOBAL__D__ZN13NetworkTables3Key11_staticLockE();
+
+void _GLOBAL__D__ZN12NetworkTable13_tableNameMapE();
+
+void _GLOBAL__D__ZN9RobotBase10m_instanceE();
+
+void _GLOBAL__D__ZN10Ultrasonic9kPingTimeE();
+
+void _GLOBAL__D_AxisCamera_debugFlag();
+
+extern void (*_dtors[])();
+void (*_dtors[])() =
+    {
+    _GLOBAL__D__Z20FRC_userClassFactoryv,
+    _GLOBAL__D__ZN9Scheduler9_instanceE,
+    _GLOBAL__D_wpi_error_s_ModuleIndexOutOfRange,
+    _GLOBAL__D__ZN13NetworkTables3Key11_staticLockE,
+    _GLOBAL__D__ZN12NetworkTable13_tableNameMapE,
+    _GLOBAL__D__ZN9RobotBase10m_instanceE,
+    _GLOBAL__D__ZN10Ultrasonic9kPingTimeE,
+    _GLOBAL__D_AxisCamera_debugFlag,
+    0
+    };
