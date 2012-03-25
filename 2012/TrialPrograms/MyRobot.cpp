1585b5f1e39984314f57762b37c9d214e4777e66
janksters.dev.A <jankster.dev.A@gmail.com>
2012-03-25 10:39:10 -0700

    Preferences test jig program.

--- 2012/TrialPrograms/MyRobot.cpp
+++ 2012/TrialPrograms/MyRobot.cpp
@@ -6,11 +6,23 @@
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
  */ 
+
+Preferences *prefs = Preferences::GetInstance();
+SmartDashboard *smarty = SmartDashboard::GetInstance();
+
+void GrabIt(const char *pName)
+{
+	float tempdouble;
+	string tempstring = smarty->GetString(pName);
+	sscanf(tempstring.c_str(),"%f",&tempdouble);
+	prefs->PutDouble(pName,(double)tempdouble);
+}
+
 class RobotDemo : public SimpleRobot
 {
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
-
+	
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
@@ -33,14 +45,59 @@ public:
 	/**
 	 * Runs the motors with arcade steering. 
 	 */
+	
 	void OperatorControl(void)
 	{
+		char *names[9] = {"P-turret","I-turret","D-turret","P-shooter","I-shooter","D-shooter","P-gyro","I-gyro","D-gyro"};
+		
+		int i;
+		
+		for(i=0; i<9; i++)
+		{
+			smarty->PutString(names[i],"0.00");	
+		}
+
+		smarty->PutString("Message","When PID values ready to save, press DISABLE");
+		
+		
+/*		std::string test = prefs->GetString("hollytest","noValue");
+		double DoubleTest = prefs->GetDouble("hollytestDouble");
+		int IntTest = prefs->GetInt("hollytestInt");
+		smarty->PutDouble("SaveDoubleTest",DoubleTest);
+		smarty->PutInt("SaveIntTest",IntTest);
+		smarty->PutString("SaveTest",test);
+		prefs->PutDouble("hollytestDouble",1.0);
+		prefs->PutInt("hollytestInt",2);
+		prefs->PutString("hollytest","TestSucceed");
+		Wait(1.0);
+		prefs->Save();
+		smarty->PutBoolean("BoolTest",false);
+*/		
+
 		myRobot.SetSafetyEnabled(true);
 		while (IsOperatorControl())
 		{
+/*			bool Test = smarty->GetBoolean("BoolTest");
+			if(Test)
+				smarty->PutString("Boolean","yes");
+			else
+				smarty->PutString("Boolean","no");
+*/
+			
+			
+			i = 0;
+			for(i=0; i<9; i++)
+				GrabIt(names[i]);
+			
+			
+			
 			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
 			Wait(0.005);				// wait for a motor update time
 		}
+		
+		prefs->Save();
+		Wait(0.5);
+		
 	}
 };
 
