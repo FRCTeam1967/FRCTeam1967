360785d6c430cf19c10bfd4fc7e44489c0f10398
Robert Wolff <bob.wolff68@gmail.com>
2014-02-22 14:26:25 -0800

    Adding the team for 2014 checkins

---
+++ docs/teammates/Megha.bat
@@ -0,0 +1,9 @@
+# Be sure to set 'name', 'email', and 'username'
+NAME="Megha Mallya"
+EMAIL="meghamallya1@gmail.com"
+GITUSERNAME="meghatron3000"
+
+git config user.name $NAME
+git config user.email $EMAIL
+git commit -a
+git push https://$GITUSERNAME@github.com/bobwolff68/FRCTeam1967
