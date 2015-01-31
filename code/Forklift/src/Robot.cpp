b1ad3ba20dacaca16d6c99654a2307b7c7f9d480
Elaine Duh <eduh18@ndsj.org>
2015-01-31 14:43:46 -0800

    Forklift test that compiles.

---
+++ code/Forklift/src/Robot.cpp
@@ -0,0 +1,63 @@
+#include "WPILib.h"
+#include "Forklift.h"
+#include "jankyXboxJoystick.h"
+// #include "jankyTask.h"
+
+#define FORKLIFT_PORT 1
+#define ROLLER_LEFT 5
+#define ROLLER_RIGHT 6
+
+class Robot: public IterativeRobot
+{
+	jankyXboxJoystick * stick;
+	JankyForklift * forky;
+
+public:
+	Robot() {
+		stick = NULL;
+		forky = NULL;
+	}
+	~Robot() {
+		delete stick;
+		delete forky;
+	}
+
+private:
+
+	void RobotInit()
+	{
+		stick = new jankyXboxJoystick(FORKLIFT_PORT);
+		forky = new JankyForklift(ROLLER_LEFT, ROLLER_RIGHT);
+	}
+
+	/*void AutonomousInit()
+	{
+
+	}
+
+	void AutonomousPeriodic()
+	{
+
+	}
+
+	void TeleopInit()
+	{
+
+	}*/
+
+	void TeleopPeriodic()
+	{
+		if (stick->GetButtonA() == true) {
+			forky->pullIn();
+		}
+		if (stick->GetButtonB() == true) {
+			forky->pushOut();
+		}
+	}
+
+	/*void TestPeriodic()
+	{
+	}*/
+};
+
+START_ROBOT_CLASS(Robot);
