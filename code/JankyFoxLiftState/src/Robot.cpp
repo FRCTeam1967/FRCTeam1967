c76dbc80b611fb9a62c8626385737f111fcd8e70
Megha Mallya <meghamallya1@gmail.com>
2015-02-22 09:45:33 -0800

    Changes to joystick that allows it to rotate (thanks Natasha)

--- code/JankyFoxLiftState/src/Robot.cpp
+++ code/JankyFoxLiftState/src/Robot.cpp
@@ -7,12 +7,18 @@
 
 class Robot: public IterativeRobot
 {
+	const static int fLeft=2;
+	const static int rLeft=4;
+	const static int fRight=1;
+	const static int rRight=3;
+	RobotDrive meghaRobot;
 	jankyXboxJoystick* gameComponent;
 	Joystick*driver;
 	JankyFoxliftState*foxlift;
 
 public:
-	Robot(){
+	Robot(): meghaRobot(fLeft, rLeft, fRight, rRight)
+	{
 		printf("in robot constructor \n");
 		gameComponent = NULL;
 		driver = NULL;
@@ -39,6 +45,8 @@ private:
 	void TeleopPeriodic()
 	{
 		SmartDashboard::PutBoolean("limit Switch Top closed", foxlift->IsLSwitchTopClosed());
+		SmartDashboard::PutNumber("Twist", driver->GetZ());
+		SmartDashboard::PutNumber("XAxis", driver->GetX());
 		SmartDashboard::PutBoolean("limit Switch Down closed", foxlift->IsLSwitchDownClosed());
 		SmartDashboard::PutNumber("Box Lift Speed", foxlift->motorLift->Get());
 		SmartDashboard::PutNumber("Intake Roller 1 Speed", foxlift->motorRoller1->Get());
@@ -47,7 +55,16 @@ private:
 		SmartDashboard::PutBoolean("Singulation IN/OUT", foxlift->singulationOne->Get());
 		SmartDashboard::PutBoolean("Singulation UP/DOWN", foxlift->singulationTwo->Get());
 		SmartDashboard::PutBoolean("Intake pistons", foxlift->rollerPistons->Get());
-
+		SmartDashboard::PutNumber("Get Throttle", driver->GetThrottle());
+		//MECANUM DRIVE
+		float yValue = driver->GetY();
+		float xValue = driver->GetX()*(-1);
+		//this is apparently changing the twist
+		float rotation = driver->GetThrottle();
+		// GetZ() apparently is changed by the lever at the bottom.
+		//driver->GetZ() = 0;
+		//driver->SetAxisChannel(Joystick::kTwistAxis, 2);
+		meghaRobot.MecanumDrive_Cartesian(xValue, yValue, rotation, 0.0);
 		//Boxlift
 		if (gameComponent->GetButtonY() == true){
 			foxlift->GoUp();
