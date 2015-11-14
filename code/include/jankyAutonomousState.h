603ccbf1e41ad28d2373037edf458bc9f25fe979
Elaine Duh <eduh18@ndsj.org>
2015-11-14 12:18:57 -0800

    committing to resolve conflict

--- code/include/jankyAutonomousState.h
+++ code/include/jankyAutonomousState.h
@@ -15,6 +15,9 @@
 #define DRIVE_FORWARD_TIME 0.2
 #define TURN_TIME 0.75
 #define DRIVE_TO_AUTO_TIME 1.2
+#define DRIVE_BACKWARD_TIME 0.2
+#define BINGULATE_TIME 2.0
+#define BIN_PISTON 5
 
 class JankyFoxliftState;
 
@@ -26,6 +29,10 @@ public:
 	//Member variables
 	enum StateValue {
 		Idle,
+		BinIdle,
+		Bingulate,
+		DriveBackToAuto,
+		Unbingulate,
 		DriveSideways,
 		DriveForward,
 		DownTote,
@@ -37,12 +44,15 @@ public:
 	};
 	Timer * driveForwardTimer;
 	Timer * driveSidewaysTimer;
+	Timer * binTimer;
+	Timer * driveBackTimer;
 	Timer * turnTimer;
 	Timer * driveToAutoTimer;
 	RobotDrive * ptRobot;
 	JankyFoxliftState * ptFoxLift;
 	bool driveOnce;
 	bool goDownOnce;
+	Solenoid * binPiston;
 	
 	//Member functions
 	void StateEngine(int curState);
@@ -53,7 +63,8 @@ public:
 	void StartAuto(void);
 	void GoTurnToAuto(void);
     void GoForwardToAuto(void);
-
+    void ExtendBingulate();
+    void RetractBingulate();
 };
 
 #endif
