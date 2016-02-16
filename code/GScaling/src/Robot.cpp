1c1b433d64c8ee55b3b73470d0c6d7e7251f79fd
Gillian McGinnis <gmcginnis18@ndsj.org>
2016-02-16 14:06:05 -0800

    for scaling group

---
+++ code/GScaling/src/Robot.cpp
@@ -0,0 +1,67 @@
+#include "WPILib.h"
+#include "JankyScaling.h"
+#include "JankyTask.h"
+#include "jankyDrivestick.h"
+#include "jankyXboxJoystick.h"
+#include "jankyEncoder.h"
+
+#define DRIVE_JOYSTICK_PORT 1
+#define GC_JOYSTICK_PORT 0
+#define SCALING_ENCODER_CHANNELA 1
+#define SCALING_ENCODER_CHANNELB 0
+#define SCALING_MOTOR_CHANNEL 1
+#define SCALING_PISTON_CHANNEL 1
+
+class Robot:public IterativeRobot{
+
+	jankyDrivestick*drivestick;			//creating pointer for jankyDrivestick to be called drivestick
+	jankyXboxJoystick*joystick;			//creating pointer for jankyXboxJoystick to be called joystick
+	jankyScaling*scaling;				//creating pointer for jankyScaling to be called scaling
+
+public:
+
+	Robot(){
+			drivestick = NULL;
+			joystick = NULL;
+			scaling = NULL;
+		}
+	~Robot(){
+
+			drivestick = NULL;
+			joystick = NULL;
+			scaling = NULL;
+
+			delete drivestick;
+			delete joystick;
+			delete scaling;
+		}
+
+private:
+	void TeleopInit(){
+		printf("Made it to RobotInit\n");
+		drivestick = new jankyDrivestick(DRIVE_JOYSTICK_PORT);				//setting drivestick to have its port at DRIVE_JOYSTICK_PORT;
+		joystick = new jankyXboxJoystick(GC_JOYSTICK_PORT);
+		scaling = new jankyScaling(SCALING_ENCODER_CHANNELA, SCALING_ENCODER_CHANNELB, SCALING_MOTOR_CHANNEL, SCALING_PISTON_CHANNEL);
+		//scaling->EncoderReset();
+	}
+
+	void TeleopPeriodic(){
+		if (joystick->GetButtonA() == true){			//scaling Release; when button A on the Xbox controller is pressed, Release will be enabled
+			printf("Going to Release now\n");
+			scaling->Release();
+		}
+		if (joystick->GetButtonB() == true){			//scaling WindUp; when button B on the Xbox controller is pressed, WindUp will be enabled
+			//scaling->MotorEncoderReset();
+			printf("Going to Wind Up now\n");
+			//scaling->EncoderReset();
+			scaling->WindUp();
+		}
+		if (joystick->GetButtonY() == true){
+			printf("Stopping WindUp\n");
+			scaling->StopWU();
+			//scaling->EncoderReset();
+		}
+	}
+};
+
+START_ROBOT_CLASS(Robot)
---
+++ code/GScaling/src/jankyScaling.cpp
@@ -0,0 +1,124 @@
+/*
+ * jankyScaling.cpp
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: Gillian
+ */
+#include "WPILib.h"
+#include "jankyScaling.h"
+#include <math.h>
+#include "jankyTask.h"
+#include "jankyEncoder.h"
+#include <cmath>
+
+#define SCALING_WINDS 16					//Scaling winch needs to turn 360 degrees exactly 16 times
+#define WIND_SPEED 1.0
+#define RELEASE_TIME 1.0
+
+//Constructor
+jankyScaling::jankyScaling(int encoderChannelA, int encoderChannelB, int motorChannel, int pistonChannel)
+{
+	piston = new Solenoid(19, pistonChannel);
+	printf("About to set piston to false\n");
+	piston->Set(false);
+
+	motorEncoder = new JankyEncoder(encoderChannelA, encoderChannelB, motorChannel);
+	motorEncoder->setSpeed(WIND_SPEED);
+	printf("Set revolution\n");
+	//windUpCancel = false;
+	//printf("Resetting encoder now\n");
+	//motorEncoder->Reset();
+
+	encoderReset = false;
+	motorEncoder->Reset();
+	motorEncoder->Start();
+
+}
+
+//Destructor
+jankyScaling::~jankyScaling()
+{
+	piston->Set(false);
+	delete piston;
+	delete motorEncoder;
+}
+
+void jankyScaling::EncoderReset(){
+	motorEncoder->Reset();
+	//motorEncoder->setSpeed(WIND_SPEED);
+	//motorEncoder->setRevolution(SCALING_WINDS);
+}
+
+void jankyScaling::MotorEncoderReset(){
+	encoderReset = true;
+}
+
+void jankyScaling::Release(){
+	printf("Releasing piston now\n");
+	piston->Set(true);												//Pushing out piston to hit the CAM; no need to bring it back in we believe
+	Wait(RELEASE_TIME);
+	piston->Set(false);
+}
+
+void jankyScaling::WindUp()
+{
+	//if (encoderReset == true){
+	motorEncoder->setRevolution(SCALING_WINDS);
+	motorEncoder->Go();
+	//motorEncoder->Reset();
+	//motorEncoder->Go();
+	//}
+
+	/*motorEncoder->setRevolution(SCALING_WINDS);
+
+	printf("a motorEncoder->getRevolution() abs(%d)", abs(motorEncoder->getRevolution()));
+	if (SCALING_WINDS > abs(motorEncoder->getRevolution())){
+	}
+	else{
+		motorEncoder->Reset();
+		printf("Winch done winding up\n");
+	}
+
+	printf("Made it to WindUp\n");
+	encoder->Reset();												//Reseting encoder to zero
+	printf("Reset encoder\n");
+	printf("About to start WindUp motor\n");
+
+	while(encoder->Get() < (SCALING_WINDS*360)){					//While the encoder is below 360 times the amount of revolutions, the motor will be set to -0.25 speed
+		motor->Set(-0.25);
+		SmartDashboard::PutNumber("Scaling Winds", encoder->Get());	//Printing encoder number on SmartDashboard
+		if(windUpCancel == true){
+			motor->Set(0);
+			windUpCancel = false;
+			encoder->Reset();
+			break;
+		}
+
+	}
+
+	motor->Set(0);													//After the encoder has hit 360, the motor will stop
+	printf("WindUp motor stopped\n");
+
+	if (encoder->Get() == 360){
+		motor->Set(0);
+		printf("Stopping WindUp motor\n");
+	}
+	else{
+		printf("Starting WindUp motor\n");
+		motor->Set(0.25);
+		Wait(10);
+	}*/
+}
+
+void jankyScaling::StopWU(){
+	motorEncoder->stopMotor();
+	motorEncoder->Reset();
+	//motorEncoder->setRevolution(SCALING_WINDS);
+	printf("Canceling all\n");
+}
+
+
+/*float jankyScaling::GetWrap()
+{
+	encoder->Get();
+}*/
