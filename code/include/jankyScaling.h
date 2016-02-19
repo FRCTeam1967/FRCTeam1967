a132afa7364fcaab01670e25ef5ac378bd8a6c4b
Gillian McGinnis <gmcginnis18@ndsj.org>
2016-02-19 15:29:45 -0800

    header file for gscaling

---
+++ code/include/jankyScaling.h
@@ -0,0 +1,47 @@
+/*
+ * jankyScaling.h
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: Gillian
+ */
+
+#ifndef INCLUDE_JANKYSCALING_H_
+#define INCLUDE_JANKYSCALING_H_
+//#define SCALING_WINDS 16					//Scaling winch needs to turn 360 degrees exactly 16 times
+
+//#include "JankyTask.h"
+#include "jankyTask.h"
+#include "WPILib.h"
+#include "jankyEncoder.h"
+
+class jankyScaling{
+public:
+	//member functions
+	jankyScaling(int encoderChannelA, int encoderChannelB, int motorChannel, int pistonChannel);
+	//jankyScaling();
+	~jankyScaling();
+	void EncoderReset();
+	void MotorEncoderReset();
+	void Release();
+	void WindUp();
+	void StopWU();
+	float GetWrap();
+
+
+	//member variables
+	/*int pistonChannel = 1;
+	int motorChannel = 2;
+	int encoderChannelA;
+	int encoderChannelB;
+	int motorChannel;
+	int pistonChannel;*/
+	//bool windUpCancel;
+	bool encoderReset;
+	Solenoid*piston;
+	JankyEncoder*motorEncoder;
+
+};
+
+
+
+#endif /* INCLUDE_JANKYSCALING_H_ */
