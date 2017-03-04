87210173063569f66bcb5e93f683bf18262fab2d
Elaine Duh <eduh18@ndsj.org>
2017-03-03 17:37:17 -0800

    Combined Anisha's updated chassis code with updated buttons from drive team practice (climbing to xbox stick, one gear/fuel button) and added some camel case

--- code/2017_Steamworks/src/Robot.cpp
+++ code/2017_Steamworks/src/Robot.cpp
@@ -15,10 +15,12 @@
 #include <cmath>
 //Channels for Jankybot
 //Chassis
+
 #define FRONT_LEFT_MOTOR_CHANNEL 1//3
 #define REAR_LEFT_MOTOR_CHANNEL 2//4
 #define FRONT_RIGHT_MOTOR_CHANNEL 3//1
 #define REAR_RIGHT_MOTOR_CHANNEL 4//2
+
 #define LPISTON_CHANNEL 0
 #define RPISTON_CHANNEL 0
 #define LPISTON_MOD 9
@@ -54,27 +56,25 @@ class Robot: public frc::IterativeRobot {
 	CANTalon*frmotor;
 	CANTalon*rrmotor;
 	jankyXboxJoystick*drivestick;
-	TwoTransmissions*twotransmissions;
+	TwoTransmissions*twoTransmissions;
 	RobotDrive*drive;
 	//Encoder*right_encoder;
 	//Encoder*left_encoder;
 	RopeClimbing * climb;
-	jankyXboxJoystick * gamecomponentxbox;
+	jankyXboxJoystick * gameComponentXbox;
 	ADXRS450_Gyro*gyro;
 	//PIDDrive*myRobot;
     float kP;
     //PIDController * PID;
     GearsFuel * gefu;
 	bool DriveXnotpressed = true;
-	bool DriveBnotpressed = true;
+	bool DriveLBnotpressed = true;
 	bool GuelXnotpressed = true;
-	//
+
+	// gears/fuel booleans
     bool AnotPressed = true;
-    bool BnotPressed = true;
-    bool YnotPressed = true;
     bool XnotPressed = true;
-    bool LBnotPressed = true;
-    bool RBnotPressed = true;
+
 	float avg;
 public:
 	Robot(){
@@ -83,12 +83,12 @@ public:
 			frmotor=NULL;
 			rrmotor=NULL;
 			drivestick=NULL;
-			twotransmissions=NULL;
+			twoTransmissions=NULL;
 			drive=NULL;
 			//right_encoder=NULL;
 			//left_encoder=NULL;
 			climb=NULL;
-			gamecomponentxbox=NULL;
+			gameComponentXbox=NULL;
 			gyro=NULL;
 	        //myRobot = NULL;
 	        kP = 0.03;
@@ -102,12 +102,12 @@ public:
 			delete frmotor;
 			delete rrmotor;
 			delete drivestick;
-			delete twotransmissions;
+			delete twoTransmissions;
 			delete drive;
 			//delete right_encoder;
 			//delete left_encoder;
 			delete climb;
-			delete gamecomponentxbox;
+			delete gameComponentXbox;
 			delete gyro;
 	        //delete myRobot;
 	        //delete PID;
@@ -120,19 +120,19 @@ public:
 			frmotor= new CANTalon(FRONT_RIGHT_MOTOR_CHANNEL);
 			rrmotor= new CANTalon(REAR_RIGHT_MOTOR_CHANNEL);
 			drivestick= new jankyXboxJoystick(DRIVESTICK_CHANNEL);
-			twotransmissions= new TwoTransmissions(LPISTON_CHANNEL, RPISTON_CHANNEL, LPISTON_MOD, RPISTON_MOD);
+			twoTransmissions= new TwoTransmissions(LPISTON_CHANNEL, RPISTON_CHANNEL, LPISTON_MOD, RPISTON_MOD);
 			drive= new RobotDrive(flmotor, rlmotor, frmotor, rrmotor);
 			//right_encoder = new Encoder(RIGHT_ENCODER_CHANNEL_A, RIGHT_ENCODER_CHANNEL_A);
 			//left_encoder = new Encoder(LEFT_ENCODER_CHANNEL_A, LEFT_ENCODER_CHANNEL_A);
 			climb = new RopeClimbing(MOTOR_A_CHANNEL, MOTOR_B_CHANNEL, ENCODER_CHANNEL_A, ENCODER_CHANNEL_B, LIMIT_SWITCH_CHANNEL);
-			gamecomponentxbox = new jankyXboxJoystick(GAME_COMPONENT_XBOX_CHANNEL);
+			gameComponentXbox = new jankyXboxJoystick(GAME_COMPONENT_XBOX_CHANNEL);
 			gyro = new ADXRS450_Gyro(SPI::Port::kOnboardCS0);
 	        //myRobot = new PIDDrive(flmotor, rlmotor, frmotor, rrmotor);
 	        gefu = new GearsFuel (GEAR_CHANNEL, INTAKE_CHANNEL, OUTTAKE_CHANNEL);
 			//right_encoder->Reset();
 			//left_encoder->Reset();
 			gyro->Calibrate();
-			twotransmissions->LowGear();
+			twoTransmissions->LowGear();
 			drive->SetSafetyEnabled(false);
 			printf("Done with robot init");
 
@@ -180,45 +180,55 @@ public:
 				}*/
 			}
 			else{ //regular driving w/out pressing button
-				drive->TankDrive(-lAxisVal,-rAxisVal);
-				//Code from squaring joystick value
-				/*if (leftYaxis<0&&rightYaxis<0){//forward driving with squared inputs for more precision
-					drive->TankDrive((pow(leftYaxis,2)), (pow(rightYaxis,2)));
-				}
-				else if((leftYaxis<=0&&rightYaxis>0) or (leftYaxis<0&&rightYaxis>=0)){//(leftYaxis<=0&&rightYaxis>0) or (leftYaxis<0&&rightYaxis>=0)
-					drive->TankDrive(pow(leftYaxis,2), (pow(rightYaxis,2)*-1));
-				}
-				else if((leftYaxis>=0&&rightYaxis<0) or (leftYaxis>0&&rightYaxis<=0)){
-					drive->TankDrive((pow(leftYaxis,2)*-1), (pow(rightYaxis,2)));
-				}
-				else{
-					drive->TankDrive((pow(leftYaxis,2)*-1), ((pow(rightYaxis,2))*-1));
-				}*/
+                drive->TankDrive(-lAxisVal,-rAxisVal);
+                //Code from squaring joystick value
+                /*if (leftYaxis<0&&rightYaxis<0){//forward driving with squared inputs for more precision
+                    drive->TankDrive((pow(leftYaxis,2)), (pow(rightYaxis,2)));
+                }
+                else if((leftYaxis<=0&&rightYaxis>0) or (leftYaxis<0&&rightYaxis>=0)){//(leftYaxis<=0&&rightYaxis>0) or (leftYaxis<0&&rightYaxis>=0)
+                    drive->TankDrive(pow(leftYaxis,2), (pow(rightYaxis,2)*-1));
+                }
+                else if((leftYaxis>=0&&rightYaxis<0) or (leftYaxis>0&&rightYaxis<=0)){
+                    drive->TankDrive((pow(leftYaxis,2)*-1), (pow(rightYaxis,2)));
+                }
+                else{
+                    drive->TankDrive((pow(leftYaxis,2)*-1), ((pow(rightYaxis,2))*-1));
+                }*/
 			}
 
 			//Manual Two Speed Transmissions
-			bool ButtonX = drivestick->GetButtonX();
-			bool ButtonB = drivestick->GetButtonB();
+			//bool ButtonX = drivestick->GetButtonX();
+			bool ButtonLB = drivestick->GetButtonLB();
 
 				//Button X for Low Gear
-				if(ButtonX&&DriveXnotpressed)
+				/*if(ButtonX&&DriveXnotpressed)
 				{
-					twotransmissions->LowGear();
+					twoTransmissions->LowGear();
 					DriveXnotpressed = false;
 				}
 				else if(!ButtonX)
 				{
 					DriveXnotpressed = true;
 				}
-				//Button B for High Gear
-				if(ButtonB&&DriveBnotpressed)
+				//Button LB for High Gear
+				if(ButtonLB&&DriveLBnotpressed)
 				{
-					twotransmissions->HighGear();
-					DriveBnotpressed = false;
+					twoTransmissions->HighGear();
+					DriveLBnotpressed = false;
 				}
-				else if(!ButtonB)
+				else if(!ButtonLB)
 				{
-					DriveBnotpressed = true;
+					DriveLBnotpressed = true;
+				}*/
+
+				// hold down LB for high gear
+				if (ButtonLB)
+				{
+					twoTransmissions->LowGear();
+				}
+				else
+				{
+					twoTransmissions->HighGear();
 				}
 
 			//Climbing Code
@@ -229,72 +239,46 @@ public:
 				SmartDashboard::PutNumber("climb motor B current: ", climb->GetMotorBCurrent());
 				SmartDashboard::PutNumber("Left Throttle", drivestick->GetLeftThrottle());
 				SmartDashboard::PutNumber("Right Throttle", drivestick->GetRightThrottle());
-				// Drivestick button A starts climbing motors
-				// TODO: Move buttons to game component joystick
-				if (drivestick->GetButtonA() && !drivestick->GetButtonY())
+				// Drivestick button Y starts climbing motors
+				// Climbing only goes as long as Y is held down
+				if (gameComponentXbox->GetButtonY() && !gameComponentXbox->GetButtonB())
 				{
 					climb->StartClimbing();
 				}
-
-				// Drivestick button Y stops climbing motors
-				if (drivestick->GetButtonY())
+				else
 				{
 					climb->StopClimbing();
 				}
 
-	//Gears and Fuel Code
-		//Button X for Box pushed out
+				// Drivestick button B stops climbing motors (very useless)
+				if (gameComponentXbox->GetButtonB())
+				{
+					climb->StopClimbing();
+				}
 
-		        if (gamecomponentxbox->GetButtonB() && BnotPressed) {
-		            gefu->GearOut();
-		            BnotPressed=false;
-		            printf("gearout \n");
-		        }
-		            else if (!gamecomponentxbox->GetButtonB()) {
-		                BnotPressed=true;
-		        }
-		        if (gamecomponentxbox->GetButtonX() && XnotPressed) {
-		            gefu->GearIn();
-		            XnotPressed=false;
-		            printf("gearin \n");
-		        }
-		        else if(!gamecomponentxbox->GetButtonX()) {
-		            XnotPressed=true;
-		        }
-		        // for intake (piston 2)
-		        if(gamecomponentxbox->GetButtonA() && AnotPressed) {
-		                gefu->Horz();
-		                AnotPressed=false;
-		                printf("horz \n");
-		            }
-		        else if (!gamecomponentxbox->GetButtonA()) {
-		            AnotPressed=true;
-		                }
-		            if (gamecomponentxbox->GetButtonY() && (YnotPressed)) {
-		                gefu->Horz2();
-		                YnotPressed=false;
-		                printf("horz2 \n");
-		            }
-		            else if(!gamecomponentxbox->GetButtonY()) {
-		                        YnotPressed=true;
-		                    }
-		        // for outtake (piston 3)
-		        if (gamecomponentxbox->GetButtonLB() && (LBnotPressed)) {
-		                gefu->Vert();
-		                LBnotPressed=false;
-		                printf("vert \n");
-		            }
-		        else if(!gamecomponentxbox->GetButtonLB()) {
-		                LBnotPressed=true;
-		            }
-		            if (gamecomponentxbox->GetButtonRB() && (RBnotPressed)) {
-		                gefu->Vert2();
-		                RBnotPressed=false;
-		                printf("vert2 \n");
-		            }
-		            else if(!gamecomponentxbox->GetButtonRB()) {
-		                            RBnotPressed=true;
-		                        }
+				//Gears and Fuel Code
+				//Button X for Box pushed out
+
+                //Gear Piston (1): Make piston go out & in with button X
+				if (gameComponentXbox->GetButtonX()) {
+					gefu->GearOut();
+				    printf("gearout \n");
+				}
+                else {
+                    gefu->GearIn();
+                }
+
+				// TODO: figure out if drive team needs this section
+/*                //Outtake Piston (2): Make piston go out & in
+                //Fuel Outtake
+                if(gameComponentXbox->GetButtonA() && AnotPressed) {
+                    gefu->Horz();
+                    AnotPressed=false;
+                }
+                else if (gameComponentXbox->GetButtonA() && !AnotPressed) {
+                    gefu->Horz2();
+                    AnotPressed=true;
+                }*/
 	}
 
 	void TestPeriodic() {
