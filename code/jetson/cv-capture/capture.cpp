24ee92111194a4a7e27af3f3757c911260ca9ae4
Anika Kartik <akartik20@ndsj.org>
2018-02-10 21:57:51 +0000

    Accurate distance formula for vision

--- code/jetson/cv-capture/capture.cpp
+++ code/jetson/cv-capture/capture.cpp
@@ -6,25 +6,44 @@
 #include <opencv2/opencv.hpp>
 #include <math.h>
 #include <cstdlib>
+//#include <cscore>
 
 using namespace std;
+using namespace cv;
 
 const int MIN_AREA =900; //pixels
-const float T_FEET_HEIGHT = 16;
-const float T_FEET_WIDTH = 2;
+const float T_INCHES_HEIGHT = 16;
+const float T_INCHES_WIDTH = 2;
+const float T_INCHES_LEFT_WIDTH = 6;
 const int FOV_PIXELS_HEIGHT = 480;
 const int FOV_PIXELS_WIDTH = 640;
 const float theta = 68.5 * 3.141592653589793238462643383279 / 360; //degrees
+const float MEASURED_HORIZ_FOV = 51.80498 * 3.141592653589793238462643383279 / 360;
+const float MEASURED_VERT_FOV = 38.3557 * 3.141592653589793238462643383279 / 360;
 
 int main()
 {
-    //system("v4l2-ctl -d /dev/video1 --set-fmt-video=width=1280,height=720,pixelformat=BGR3");
-//    SetResolution(1280, 720);
+	cout << "set fmt before cap(1)" << endl;
+    	system("v4l2-ctl -d /dev/video1 --verbose --set-fmt-video=width=1280,height=720,pixelformat=1");
 //    system("ffprobe -f v4l2 -input_format mjpeg -video_size 1280x720 /dev/video1");
-    //system("v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=1 -c brightness=30");
-	system("v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=10 -c brightness=50");
-    cv::VideoCapture cap(1);
+	system("v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=1 -c brightness=10"); // KEEP
+	//system("v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=10 -c brightness=50");
+	cout << "get fmt before cap(1)" << endl;
+	system("v4l2-ctl -d /dev/video1 --get-fmt-video");
+	cout << "calling cap(1)" << endl;
+        VideoCapture cap(1);
+	//cs::CvSource = cs::CvSource("src", cs::VideoMode::PixelFormat::kMJPEG, 1280, 640, 15);
+	system("v4l2-ctl -d /dev/video1 --verbose --set-fmt-video=width=1280,height=720,pixelformat=BGR3");
+//	VideoCapture cap("nvcamerasrc ! video/x-raw(memory:NVM), width=(int)1280, height=(int)720, format=(string)I420, framerate=(fraction)24/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink");
+//	VideoCapture cap("v4l2src ! device=/dev/video1 video/x-raw, width=(int)1280, height=(int)720, format=(string)RGB ! videoconvert ! appsink");
+	system("v4l2-ctl -d /dev/video1 --get-fmt-video");
 
+	cout << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
+	cout << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+	cout << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
+	cout << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
     if (!cap.isOpened())
 	{
             cerr << "Failed to open USB camera" << endl;
@@ -33,9 +52,9 @@ int main()
 
     auto start = chrono::high_resolution_clock::now();
     float frames = 0;
-	//vector<vector<cv::Point>> contours;
-	vector<cv::Point> maxContour;
-	vector<vector<cv::Point>> contours;
+	//vector<vector<Point>> contours;
+	vector<Point> maxContour;
+	vector<vector<Point>> contours;
 
 	//vector<Vec4i> lines;
 	double hue[] = {55,85};
@@ -48,56 +67,61 @@ int main()
 
      for(;;)
      {
-	cv::Mat gray, frame, green, outline;
-
+	Mat gray, frame, green, outline;
+//	cout << "height: " << cap.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+//	cout << "width: " << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
         cap >> frame; 
 //	cout << "width get " << cap.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
 //	cout << "height: " << frame.size().height << endl;
 //	cout << "width: " << frame.size().width << endl;
 	imwrite("/home/nvidia/master-ocv/cv-capture/TapeButton.jpg", frame);
 	// convert from brg to hsv
-	cv::cvtColor(frame, green, cv::COLOR_BGR2HSV);
-	cv::inRange(green,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), green); // filter green tape
-	cv::GaussianBlur(green, outline, cv::Size(9, 9), 2, 2); // blurs image
-	cv::findContours(outline, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0)); // finds contourss
-	vector<vector<cv::Point>> contours_poly(contours.size());
-	vector<cv::Rect> boundRect(contours.size());
+	cvtColor(frame, green, COLOR_BGR2HSV);
+	inRange(green,Scalar(hue[0], sat[0], val[0]), Scalar(hue[1], sat[1], val[1]), green); // filter green tape
+	GaussianBlur(green, outline, Size(9, 9), 2, 2); // blurs image
+	findContours(outline, contours, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0)); // finds contourss
+	vector<vector<Point>> contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
 	int largestContour = 0;
+	int largestContour2 = 0;
 
 	// loops through each contour - does more processing if contour area is greater than MIN_AREA
 	for(int c = 0; c < contours.size(); c++) {
-		//double currentArea = cv::contourArea(contours[c]);
+		//double currentArea = contourArea(contours[c]);
 
-		if(cv::contourArea(contours[c]) < MIN_AREA) 
+		if(contourArea(contours[c]) < MIN_AREA) 
 		{
 //			cout << "skipped" << c <<endl;
 			continue;
 		}
 		// draws contours of random colors
-		cv::Scalar color = cv::Scalar((rand()%255)+1, (rand()%255)+1, (rand()%255)+1);
-		cv::drawContours(frame, contours, c, color, 4);
+		Scalar color = Scalar((rand()%255)+1, (rand()%255)+1, (rand()%255)+1);
+		drawContours(frame, contours, c, color, 4);
 
 		// finds polygons
-		cv::approxPolyDP(cv::Mat(contours[c]), contours_poly[c], 10, true);
+		approxPolyDP(Mat(contours[c]), contours_poly[c], 10, true);
 		for (int i=0; i<contours_poly[c].size(); i++)
 		{
-			cv::Scalar red = cv::Scalar(255, 0, 0);
-			cv::circle(frame, contours_poly[c][i], 3, red, 10);
-			cv::Point p = contours_poly[c][i];
-			cv::putText(frame, cv::format("(%d, %d)", p.x, p.y), p, cv::FONT_HERSHEY_PLAIN, 1, cv::Scalar(0, 0, 255));
+			Scalar red = Scalar(255, 0, 0);
+			circle(frame, contours_poly[c][i], 3, red, 10);
+			Point p = contours_poly[c][i];
+			putText(frame, format("(%d, %d)", p.x, p.y), p, FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
 		}
 
-		boundRect[c] = cv::boundingRect(cv::Mat(contours_poly[c]));
-		cv::rectangle(frame, boundRect[c].tl(), boundRect[c].br(), color);
+		boundRect[c] = boundingRect(Mat(contours_poly[c]));
+		rectangle(frame, boundRect[c].tl(), boundRect[c].br(), color);
 
-		if(cv::contourArea(contours[c]) > cv::contourArea(contours[largestContour]))
+		if(contourArea(contours[c]) > contourArea(contours[largestContour]))
 		{
 			largestContour = c;
 		}
+		else if (contourArea(contours[c]) > contourArea(contours[largestContour2])) {
+			largestContour2 = c;
+		}
 	}
-	
+		
 	if (contours_poly.size() > 0) {
-		cout<<"area of largest contour: - "<<cv::contourArea(contours[largestContour])<<endl;
+		cout<<"area of largest contour: - "<<contourArea(contours[largestContour])<<endl;
 		cout<<contours_poly[largestContour]<<endl;
 		cout<<"Largest rectangle height: "<<boundRect[largestContour].height <<endl;
 
@@ -110,10 +134,15 @@ int main()
 		//float distance = 220-(​1.23*​rectHeight)+(​0.00208*​pow(rectHeight, 2));
 		//cout << "parabolic distance: " << distance << endl;
 
-		if(rectWidth < 33) {
+		Rect largestRect = boundRect[largestContour];
+		Rect largestRect2 = boundRect[largestContour2];
+		float leftCornerDist = abs(largestRect.tl().x - largestRect2.tl().x);
+		float rightCornerDist = abs(largestRect.br().x - largestRect2.br().x);
+
+		if(rectWidth < 44) {
 			cout<<"frame width: "<<frame.size().width<<endl;
 			cout<<"frame height: "<<frame.size().height<<endl;
-			float fovHeight = FOV_PIXELS_HEIGHT * T_FEET_HEIGHT / rectHeight;
+			float fovHeight = FOV_PIXELS_HEIGHT * T_INCHES_HEIGHT / rectHeight;
 			cout<<"Field of view height: "<<fovHeight<<endl;
 			float fovWidth = fovHeight * frame.size().width / frame.size().height;
 			cout<<"Field of view width: "<<fovWidth<<endl;
@@ -121,12 +150,15 @@ int main()
 			cout<<"Field of view diagonal: "<<fovDiagonal<<endl;
 			float distanceToTape = fovDiagonal / (2 * tan(theta / 1.15));
 			//float distanceToTape = fovDiagonal / (2 * tan(theta));
-			cout<<"Distance to tape: "<<distanceToTape<<endl;
+			cout<<"Diagonal distance to tape: "<<distanceToTape<<endl;
+			//float verticalDistanceToTape = (T_INCHES_HEIGHT * fovHeight) / (2 * rectHeight * tan(MEASURED_VERT_FOV));
+			float verticalDistanceToTape = fovHeight / (2 * tan(MEASURED_VERT_FOV));
+			cout << "Vertical distance to tape: " << verticalDistanceToTape << endl;
 		}
 		else {
 			cout<<"frame width: "<<frame.size().width<<endl;
 			cout<<"frame height: "<<frame.size().height<<endl;
-			float fovWidth = FOV_PIXELS_WIDTH * T_FEET_WIDTH / rectWidth;
+			float fovWidth = FOV_PIXELS_WIDTH * T_INCHES_WIDTH / rectWidth;
 			cout<<"Field of view width: "<< fovWidth <<endl;
 			float fovHeight = fovWidth * frame.size().height / frame.size().width;
 			cout<<"Field of view height: "<<fovHeight<<endl;
@@ -134,34 +166,50 @@ int main()
 			cout<<"Field of view diagonal: "<<fovDiagonal<<endl;
 			float distanceToTape = fovDiagonal / (2 * tan(theta / 1.4));
 			cout<<"Distance to tape: "<<distanceToTape<<endl;
+
+			float horizDistanceToTape = (T_INCHES_WIDTH * fovWidth) / (2 * rectWidth * tan(MEASURED_HORIZ_FOV));
+			//float horizDistanceToTape = fovWidth / (2 * tan(MEASURED_HORIZ_FOV));
+			cout << "Horizontal distance to tape: " << horizDistanceToTape << endl;
+			
+			fovWidth = FOV_PIXELS_WIDTH * T_INCHES_LEFT_WIDTH / leftCornerDist;
+			//float horizDistanceToTapeLeft = (T_INCHES_LEFT_WIDTH * fovWidth) / (2 * rectHeight * tan(MEASURED_HORIZ_FOV));
+			float horizDistanceToTapeLeft = fovWidth / (2 * tan(MEASURED_HORIZ_FOV));
+			cout << "Horizontal left distance to tape: " << horizDistanceToTapeLeft << endl;
+
+			fovWidth = FOV_PIXELS_WIDTH * T_INCHES_LEFT_WIDTH / rightCornerDist;
+			//float horizDistanceToTapeLeft = (T_INCHES_LEFT_WIDTH * fovWidth) / (2 * rectHeight * tan(MEASURED_HORIZ_FOV));
+			float horizDistanceToTapeRight = fovWidth / (2 * tan(MEASURED_HORIZ_FOV));
+			cout << "Horizontal right distance to tape: " << horizDistanceToTapeRight << endl;
+
+			cout << "Horizontal distance average: " << (horizDistanceToTapeLeft + horizDistanceToTapeRight) / 2 << endl;
 		}
 	}
 	// new frame from camera
-        // cv::Sobel(frme, gray, CV_8U, 1, 1);
-        //cv::cvtColor(frame, gray, cv::COLOR_RGB2GRAY);
-	//cv::Canny(gray, frame, 35, 125);
-        // cv::fastNlMeansDenoising(gray, frame);
+        // Sobel(frme, gray, CV_8U, 1, 1);
+        // cvtColor(frame, gray, COLOR_RGB2GRAY);
+	// Canny(gray, frame, 35, 125);
+        // fastNlMeansDenoising(gray, frame);
 
-        // cv::fastNlMeansDenoising(gray, frame);
-	//cv::findContours(green, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
-	//cv::max(contours, contours, maxContour);
+        // fastNlMeansDenoising(gray, frame);
+	// findContours(green, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+	// max(contours, contours, maxContour);
 	
-	//return cv::minAreaRect(contours);
+	//return minAreaRect(contours);
 
-        // cv::imshow("camera feed", gray);
-	cv::circle(frame, cv::Point(0, 0), 3, cv::Scalar(255, 0, 0), 10);
-	cv::imshow("camera feed", frame);
-        cv::imshow("filtered green", green);
-	//cv::imshow("blurred image", outline);
+        // imshow("camera feed", gray);
+	circle(frame, Point(0, 0), 3, Scalar(255, 0, 0), 10);
+	imshow("camera feed", frame);
+        imshow("filtered green", green);
+	//imshow("blurred image", outline);
 
 #if 1
-        // char c = cv::waitKey(30);
-        char c = cv::waitKey(1);
+        // char c = waitKey(30);
+        char c = waitKey(1);
 
         if (c == ' ')
             break;
 #else
-        char c = cv::waitKey(1);
+        char c = waitKey(1);
 #endif
         frames++;
     }
