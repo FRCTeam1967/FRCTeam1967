fb7d23b0ee048b1ed52dfff6b106def73c70e119
Anisha Kabir <ahsina.ribak@gmail.com>
2018-08-19 20:11:09 -0700

    Inverted motors on right side of robot so that all the motors drove in the same direction during autonomous; at the beginning of each autonomous test the robot would left turn for ~30 degrees and then continue to drive straight without stopping long after the target position was reached; but switches from autonomous to teleop and back worked well without conflicts between the drive and the talons

--- code/TalonSRXPositionControl/src/Robot.cpp
+++ code/TalonSRXPositionControl/src/Robot.cpp
@@ -25,11 +25,10 @@
 
 //removed scale factor to see accuracy
 //#define SCALE_FACTOR 1.25
-#define MEASURED_DIST_PER_PULSE 0.0912
 #define ENCODER_UNITS_PER_ROTATION 4096
 #define DIAMETER 6.25
 #define CIRCUMFERENCE_INCHES DIAMETER*M_PI//*SCALE_FACTOR
-#define DRIVE_PID_TIMER 3
+#define DRIVE_PID_TIMER 1
 
 class Robot : public frc::IterativeRobot {
 	WPI_TalonSRX*flmotor;
@@ -37,7 +36,7 @@ class Robot : public frc::IterativeRobot {
 	WPI_TalonSRX*frmotor;
 	WPI_TalonSRX*rrmotor;
 	jankyXboxJoystick*driveStick;
-	frc::ADXRS450_Gyro*gyro;
+	//frc::ADXRS450_Gyro*gyro;
 	frc::Timer autonomousTimer;
 	frc::RobotDrive*drive;
 
@@ -55,7 +54,7 @@ public:
 		frmotor=NULL;
 		rrmotor=NULL;
 		driveStick=NULL;
-		gyro=NULL;
+		//gyro=NULL;
 		drive=NULL;
 	}
 
@@ -65,7 +64,7 @@ public:
 		delete frmotor;
 		delete rrmotor;
 		delete driveStick;
-		delete gyro;
+		//delete gyro;
 	}
 
 	void RobotInit() {
@@ -75,15 +74,19 @@ public:
 		rrmotor = new WPI_TalonSRX(REAR_RIGHT_MOTOR_CHANNEL);
 		driveStick = new jankyXboxJoystick(JOYSTICK_CHANNEL);
 
-		flmotor->ConfigSelectedFeedbackSensor(CTRE_MagEncoder_Absolute, 0, 10);
+		frmotor->SetInverted(true);
+		rrmotor->SetInverted(true);
+
 		frmotor->ConfigSelectedFeedbackSensor(CTRE_MagEncoder_Absolute, 0, 10);
 
-		frmotor->ConfigPeakOutputForward(0.5, 10); //configure back to max before teleop
-		frmotor->ConfigPeakOutputReverse(-0.5, 10);
+		frmotor->SetSelectedSensorPosition(0, 0, 10);
+
+		frmotor->ConfigPeakOutputForward(0.4, 10); //configure back to max before teleop
+		frmotor->ConfigPeakOutputReverse(-0.4, 10);
 		frmotor->ConfigNominalOutputForward(0, 10);
 		frmotor->ConfigNominalOutputReverse(0, 10);
 
-		frmotor->Config_kP(0, 0.1, 10);
+		frmotor->Config_kP(0, 0.06, 10);
 		frmotor->Config_kI(0, 0.0, 10);
 		frmotor->Config_kD(0, 0.0, 10);
 		frmotor->Config_kF(0, 0.0, 10);
@@ -96,20 +99,32 @@ public:
 			drive=NULL;
 		}
 
-		inchDistance=48;
-		target = (inchDistance/CIRCUMFERENCE_INCHES)*ENCODER_UNITS_PER_ROTATION; //converting inches to encoder counts
-		lEncoderCount = -flmotor->GetSensorCollection().GetQuadraturePosition();
-		rEncoderCount = frmotor->GetSensorCollection().GetQuadraturePosition();
+		//startRightEncoderCount=frmotor->GetSensorCollection().GetQuadraturePosition();
 
-		frmotor->Set(ControlMode::Position, target);
+		inchDistance=80;
+		target = (inchDistance/CIRCUMFERENCE_INCHES)*ENCODER_UNITS_PER_ROTATION; //converting inches to encoder counts
+		//printf("Left Encoder distance %f Right Encoder distance %f \n", (lEncoderCount/ENCODER_UNITS_PER_ROTATION)*CIRCUMFERENCE_INCHES, (rEncoderCount/ENCODER_UNITS_PER_ROTATION)*CIRCUMFERENCE_INCHES);
+
 		flmotor->Set(ControlMode::Follower, 5); //5 for id of motor to follow
 		rrmotor->Set(ControlMode::Follower, 5);
 		rlmotor->Set(ControlMode::Follower, 5);
-
+		frmotor->Set(ControlMode::Position, target);
 	}
 
 	void AutonomousPeriodic() {
-		if(lEncoderCount>=target && rEncoderCount>=target && !distReached){
+		lEncoderCount = -flmotor->GetSensorCollection().GetQuadraturePosition();
+		rEncoderCount = frmotor->GetSensorCollection().GetQuadraturePosition();
+
+		//printf("FL Motor Output %f \n", flmotor->Get());
+		//printf("FR Motor Output %f \n", frmotor->Get());
+		//printf("RL Motor Output %f \n", rlmotor->Get());
+		//printf("RR Motor Output %f \n", rrmotor->Get());
+
+
+		if(!distReached){
+			//printf("Left Encoder distance %f Right Encoder distance %f \n", (lEncoderCount/ENCODER_UNITS_PER_ROTATION)*CIRCUMFERENCE_INCHES, (rEncoderCount/ENCODER_UNITS_PER_ROTATION)*CIRCUMFERENCE_INCHES);
+		}
+		if(rEncoderCount>=target && !distReached){
 			autonomousTimer.Start(); //timer for pid to stabilize once it reaches target
 			distReached=true;
 			printf("dist reached \n");
@@ -135,11 +150,16 @@ public:
 		rrmotor->Set(ControlMode::PercentOutput, 0);
 		rlmotor->Set(ControlMode::PercentOutput, 0);
 
+		frmotor->ConfigPeakOutputForward(1.0, 10);
+		frmotor->ConfigPeakOutputReverse(-1.0, 10);
+		frmotor->ConfigNominalOutputForward(0, 10);
+		frmotor->ConfigNominalOutputReverse(0, 10);
+
 		drive->SetSafetyEnabled(false);
 	}
 
 	void TeleopPeriodic() {
-		drive->TankDrive(-driveStick->GetLeftYAxis(), -driveStick->GetRightYAxis());
+		drive->TankDrive(-driveStick->GetLeftYAxis(), driveStick->GetRightYAxis());
 	}
 
 	void TestPeriodic() {}
