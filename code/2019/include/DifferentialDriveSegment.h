745820c1195e432d0b67598a7211bd4c6af39469
Anisha Kabir <ahsina.ribak@gmail.com>
2019-01-27 11:54:48 -0800

    Header files for DifferentialDriveSegment and DifferentialTurnSegment

---
+++ code/2019/include/DifferentialDriveSegment.h
@@ -0,0 +1,74 @@
+/*
+ * DriveSegment.h
+
+ *
+ *  Created on: Feb 8, 2018
+ *      Author: Anisha Kabir
+ */
+
+#ifndef DIFFERENTIALDRIVESEGMENT_H_
+#define DIFFERENTIALDRIVESEGMENT_H_
+#include "JankyAutoEntry.h"
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+
+class DifferentialDriveSegment : public JankyAutoEntry, public frc::PIDOutput{
+public:
+	DifferentialDriveSegment(frc::ADXRS450_Gyro*gyro, frc::DifferentialDrive*drive, SensorCollection*leftEncoder, SensorCollection*rightEncoder, WPI_TalonSRX*leftmotor, WPI_TalonSRX*rightmotor, int inchDistance, double speed, double p, double i, double d);
+	//DriveSegment(RobotDrive*drive, Encoder*testEncoder, int inchDistance, double speed);
+	virtual ~DifferentialDriveSegment();
+
+protected:
+	/**
+	 * @brief Returns whether the turn is complete or not
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	bool JobDone();
+
+	/**
+	 * @brief is called repeatedly by jankyAutoEntry by IsComplete()
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void RunAction();
+
+	/**
+	 * @brief One time action for preparing or starting
+	 * sets the encoders
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void Start();
+
+	/**
+	 * @brief disable the chassis
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void End();
+
+private:
+	double kP;
+	double kI;
+	double kD;
+	double maxTime;
+	bool encoderReset;
+	double leftDist;
+	double rightDist;
+	frc::Timer*encoderTimer;
+	frc::Timer*armTimer;
+	frc::ADXRS450_Gyro*_gyro;
+	frc::PIDController*pid;
+	frc::DifferentialDrive*chassis;
+	SensorCollection*_leftEncoder;
+	SensorCollection*_rightEncoder;
+	WPI_TalonSRX*_leftmotor;
+	WPI_TalonSRX*_rightmotor;
+	frc::Encoder*_encoder;
+	int distance;
+	double _speed;
+	void PIDWrite(double output);
+};
+
+#endif /* DRIVESEGMENT_H_ */
---
+++ code/2019/include/DifferentialTurnSegment.h
@@ -0,0 +1,72 @@
+/*
+ * TurnSegment.h
+
+ *
+ *  Created on: Feb 10, 2018
+ *      Author: Maxine, Bob W
+ */
+
+#ifndef SRC_DIFFERENTIALTURNSEGMENT_H_
+#define SRC_DIFFERENTIALTURNSEGMENT_H_
+
+#include "frc/WPILib.h"
+#include "JankyAutoEntry.h"
+
+
+class DifferentialTurnSegment : public JankyAutoEntry, public frc::PIDOutput{
+public:
+
+	DifferentialTurnSegment(frc::ADXRS450_Gyro*gyro, frc::DifferentialDrive*chassis, double degreeOfTurn, double speed, double p, double i, double d);
+	virtual ~DifferentialTurnSegment();
+
+
+protected:
+	/**
+	 * @brief One time action for preparing or starting
+	 * Resets the Gyro and sets the PID
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void Start();
+
+	/**
+	 * @brief Returns whether the turn is complete or not
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	bool JobDone();
+
+	/**
+	 * @brief is called repeatedly by jankyAutoEntry by IsComplete()
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void RunAction();
+
+
+
+	/**
+	 * @brief disables the PID
+	 * inherited from JankyAutoEntry
+	 * called by the JankyAutoEntry
+	 */
+	void End();
+
+private:
+	frc::Timer*turnTimer;
+	frc::PIDController*pid;
+	frc::ADXRS450_Gyro*_gyro;
+	frc::DifferentialDrive*drive;
+	int c;//c is counter
+	double targetAngle;
+	double _speed;
+	double kP;
+	double kI;
+	double kD;
+	/**
+	 * @brief makes the chassis a PIDOutput
+	 */
+	void PIDWrite(double output);
+};
+
+#endif /* SRC_TURNSEGMENT_H_ */
