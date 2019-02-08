6f4e59d43c2bb18807e65a21a52b556f12c4df1f
Maddie Waldie <maddiewaldie@me.com>
2019-02-07 19:35:34 -0800

    fixed some vision errors; still needs work

---
+++ code/2018/jetson/cv-capture/build/CMakeFiles/cv_capture.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_capture.dir/capture.cpp.o"
+  "CMakeFiles/cv_capture.dir/Contour.cpp.o"
+  "CMakeFiles/cv_capture.dir/ContourPair.cpp.o"
+  "cv_capture.pdb"
+  "cv_capture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
