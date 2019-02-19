bc35bc442434e585984044f0adc9477310cb3cfa
Sandhya Ganesan <sganesan21@ndsj.org>
2019-02-19 15:08:42 -0800

    fixed jankybuttonpanel files ( w/ limit switches + 2 joysticks)

--- code/2019/classes/JankyButtonPanel.cpp
+++ code/2019/classes/JankyButtonPanel.cpp
@@ -9,64 +9,100 @@
 
 //include ports here
 
-jankyButtonPanel::jankyButtonPanel(int portNumber) : Joystick (portNumber){}
+jankyButtonPanel::jankyButtonPanel(int joystickPortA, int joystickPortB, int elevatorBottomLS, int elevatorTopLS, int cargoBottomLS, int cargoTopLS){
+    joystick1 = new frc::Joystick(joystickPortA);
+    joystick2 = new frc::Joystick(joystickPortB);
+    elevatorBottomLimSwitch = new frc::DigitalInput(elevatorBottomLS);
+    elevatorTopLimSwitch = new frc::DigitalInput(elevatorTopLS);
+    cargoBottomLimSwitch = new frc::DigitalInput(cargoBottomLS);
+    cargoTopLimSwitch = new frc::DigitalInput(cargoBottomLS);
+    //cargoLeftLimSwitch = new frc::DigitalInput(cargoLeftLS);
+    //cargoRightLimSwitch = new frc::DigitalInput(cargoRightLS);
+    x=0;
+    y=0;
+}
 
-jankyButtonPanel::~jankyButtonPanel(){}
+jankyButtonPanel::~jankyButtonPanel(){
+    delete joystick1;
+    delete joystick2;
+    delete elevatorBottomLimSwitch;
+    delete elevatorTopLimSwitch;
+    delete cargoBottomLimSwitch;
+    delete cargoTopLimSwitch;
+    //delete cargoLeftLimSwitch;
+    //delete cargoRightLimSwitch;
+}
 
 bool jankyButtonPanel::GetRocketCargoMed(){
-    return Joystick::GetRawButton(ROCKET_MED_CARGO_PORT);
+    return joystick1 -> GetRawButton(ROCKET_MED_CARGO_PORT);
 }
 
 bool jankyButtonPanel::GetRocketCargoLow(){
-    return Joystick::GetRawButton(ROCKET_LOW_CARGO_PORT);
+    return joystick1 -> GetRawButton(ROCKET_LOW_CARGO_PORT);
 }
 
 bool jankyButtonPanel::GetRocketCargoHigh(){
-    return Joystick::GetRawButton(ROCKET_HIGH_CARGO_PORT);
+    return joystick1 -> GetRawButton(ROCKET_HIGH_CARGO_PORT);
 }
 
 bool jankyButtonPanel::GetRocketHatchLow(){ //human player station, cargo ship hatch
-    return Joystick::GetRawButton(ROCKET_LOW_HATCH_PORT);
+    return joystick2 -> GetRawButton(ROCKET_LOW_HATCH_PORT);
 }
 
 bool jankyButtonPanel::GetRocketHatchMed(){
-    return Joystick::GetRawButton(ROCKET_MED_HATCH_PORT);
+    return joystick2 -> GetRawButton(ROCKET_MED_HATCH_PORT);
 }
 
 bool jankyButtonPanel::GetRocketHatchHigh(){
-    return Joystick::GetRawButton(ROCKET_HIGH_HATCH_PORT);
+    return joystick2 -> GetRawButton(ROCKET_HIGH_HATCH_PORT);
 }
 
 bool jankyButtonPanel::GetShipCargo(){
-    return Joystick::GetRawButton(SHIP_CARGO_PORT);
+    return joystick1 -> GetRawButton(SHIP_CARGO_PORT);
 }
 
 bool jankyButtonPanel::GetGroundHeight(){
-    return Joystick::GetRawButton(GROUND_PORT);
+    return joystick2 -> GetRawButton(GROUND_PORT);
 }
 
 float jankyButtonPanel::GetElevatorYAxis(){
-	float x = (GetRawAxis(ELEVATOR_Y_AXIS_CHANNEL));
-	return x;
+    if (elevatorBottomLimSwitch -> Get()){
+        x = -(elevatorBottomLimSwitch -> Get());
+    }
+    else if (elevatorTopLimSwitch -> Get()){
+        x = elevatorTopLimSwitch -> Get();
+    }
+    else {
+        x = 0;
+    }
+    return x;
 }
 
 bool jankyButtonPanel::GetCargoIn(){
-    return Joystick::GetRawButton(CARGO_IN_PORT);
+    return joystick1 -> GetRawButton(CARGO_IN_PORT);
 }
 
 bool jankyButtonPanel::GetCargoOut(){
-    return Joystick::GetRawButton(CARGO_OUT_PORT);
+    return joystick1 -> GetRawButton(CARGO_OUT_PORT);
 }
 
 float jankyButtonPanel::GetRollersYAxis(){
-    float y = (GetRawAxis(CARGO_ROLLERS_Y_AXIS_CHANNEL));
-	return y;
+    if (cargoTopLimSwitch -> Get()){
+        y = -(cargoTopLimSwitch -> Get());
+    }
+    else if (cargoTopLimSwitch -> Get()){
+        y = cargoTopLimSwitch -> Get();
+    }
+    else {
+        y = 0;
+    }
+    return y;
 }
 
 bool jankyButtonPanel::GetBottomPistons(){
-    return Joystick::GetRawButton(BOTTOM_PISTONS_PORT);
+    return joystick2 -> GetRawButton(BOTTOM_PISTONS_PORT);
 }
 
 bool jankyButtonPanel::GetTopPistons(){
-    return Joystick::GetRawButton(TOP_PISTONS_PORT);
+    return joystick2 -> GetRawButton(TOP_PISTONS_PORT);
 }
--- code/2019/include/JankyButtonPanel.h
+++ code/2019/include/JankyButtonPanel.h
@@ -12,24 +12,24 @@
 
 // include port numbers when we have them
 
-#define ROCKET_MED_CARGO_PORT 0
-#define ROCKET_LOW_CARGO_PORT 0
-#define ROCKET_HIGH_CARGO_PORT 0
-#define ROCKET_LOW_HATCH_PORT 0//hp station, cargo ship hatch
-#define ROCKET_MED_HATCH_PORT 0
-#define ROCKET_HIGH_HATCH_PORT 0
-#define SHIP_CARGO_PORT 0
-#define GROUND_PORT 0
+#define ROCKET_MED_CARGO_PORT 4
+#define ROCKET_LOW_CARGO_PORT 6
+#define ROCKET_HIGH_CARGO_PORT 2
+#define ROCKET_LOW_HATCH_PORT 6//hp station, cargo ship hatch
+#define ROCKET_MED_HATCH_PORT 8
+#define ROCKET_HIGH_HATCH_PORT 10
+#define SHIP_CARGO_PORT 8
+#define GROUND_PORT 12
 #define ELEVATOR_Y_AXIS_CHANNEL 0
-#define CARGO_IN_PORT 0
-#define CARGO_OUT_PORT 0
+#define CARGO_IN_PORT 10
+#define CARGO_OUT_PORT 12
 #define CARGO_ROLLERS_Y_AXIS_CHANNEL 0
-#define BOTTOM_PISTONS_PORT 0
-#define TOP_PISTONS_PORT 0
+#define BOTTOM_PISTONS_PORT 4
+#define TOP_PISTONS_PORT 2
 
-class jankyButtonPanel : public frc::Joystick {
+class jankyButtonPanel {
 public:
-    jankyButtonPanel(int portNumber);
+    jankyButtonPanel(int joystickPortA, int joystickPortB, int elevatorBottomLS, int elevatorTopLS, int cargoBottomLS, int cargoTopLS);
 	virtual ~jankyButtonPanel(void);
 
     bool GetRocketCargoLow();
@@ -41,10 +41,24 @@ public:
     bool GetShipCargo();
     bool GetGroundHeight();
     float GetElevatorYAxis();
+    //bool GetElevatorLimSwitches();
     bool GetCargoIn();
     bool GetCargoOut();
     float GetRollersYAxis();
+    //bool GetCargoLimSwitches();
     bool GetBottomPistons();
     bool GetTopPistons();
+    float x;
+    float y;
+
+private:
+    frc::Joystick * joystick1;
+    frc::Joystick * joystick2;
+    frc::DigitalInput * elevatorBottomLimSwitch;
+    frc::DigitalInput * elevatorTopLimSwitch;
+    frc::DigitalInput * cargoBottomLimSwitch;
+    frc::DigitalInput * cargoTopLimSwitch;
+    //frc::DigitalInput * cargoLeftLimSwitch;
+    //frc::DigitalInput * cargoRightLimSwitch;
 };
 #endif
