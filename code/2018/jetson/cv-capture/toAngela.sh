a30ff18259f6ee5e563fd14ba2f5addc38591da2
Randy Leberknight <rleberknight@gmail.com>
2020-01-19 16:39:48 -0800

    Added scripts to switch git ID for vision team

---
+++ code/2018/jetson/cv-capture/toAngela.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git config --global user.name "Angela Zheng"
+git config --global user.email "azheng20@ndsj.org"
+git config user.name
+git config user.email
---
+++ code/2020/projects/vision/toRandy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git config --global user.name "Randy Leberknight"
+git config --global user.email "rleberknight@gmail.com"
+git config user.name
+git config user.email
