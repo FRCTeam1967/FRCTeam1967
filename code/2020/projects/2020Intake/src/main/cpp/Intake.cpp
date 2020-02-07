5ba70fa0e695eb8149624c7e8445a46fded1d313
Victoria Martelli <vmartelli21@ndsj.org>
2020-02-06 19:06:07 -0800

    test commit
    Signed-off-by: Victoria Martelli <vmartelli21@ndsj.org>

---
+++ code/2020/projects/2020Intake/src/main/cpp/Intake.cpp
@@ -0,0 +1,58 @@
+#include "frc/WPILib.h"
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+
+using namespace std;
+using namespace frc;
+
+class Robot : public frc::TimedRobot {
+  public:
+  //constructor
+  Robot()
+  {
+
+  }
+  //deconstructor
+  ~Robot()
+  {
+
+  }
+  
+  virtual void RobotInit() override
+  {
+
+  }
+
+  virtual void AutonomousInit() override
+  {
+  
+  }
+
+  virtual void AutonomousPeriodic() override
+  {
+    
+  }
+
+  virtual void TeleopInit() override
+  {
+
+  }
+
+  virtual void TeleopPeriodic() override
+  {
+    cout << "hello" << endl;
+
+  }
+
+  virtual void TestPeriodic() override
+  {
+
+  }
+
+private:
+};
+int main() 
+{ 
+  return frc::StartRobot<Robot>(); 
+}
