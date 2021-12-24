6a2acbbb1d9221440c34fac9c44b2bcbe0206597
TiffanyN22 <tnguyen22@ndsj.org>
2021-12-23 19:48:43 -0800

    moved JankyTask code and test into code-java folder

--- code-java/jankyLibExamples/jankyTaskEarlyTest/Main.java
+++ code-java/jankyLibExamples/jankyTaskEarlyTest/Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("This code is outside of the thread");
-    Test test1 = new Test();
 
     Task500 task500Test = new Task500();
     Task1000 task1000Test = new Task1000();
