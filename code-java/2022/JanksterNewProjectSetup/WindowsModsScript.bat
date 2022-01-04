957d82346b7f953bfcfdf3879deff778bfc0521b
Robert Wolff <bob.wolff68@gmail.com>
2022-01-03 22:57:23 -0800

    Tested Windows script in unit-test style
    Was not able to setup a full VSCode environment on my Windows box. But did test the batch file running from "MultiClassTest\" and saw that it does create the proper org/janksters tree, copies the files, and makes the area read-only so no editing / saving can happen accidentally in src\main\java\org\janksters\*

--- code-java/2022/JanksterNewProjectSetup/WindowsModsScript.bat
+++ code-java/2022/JanksterNewProjectSetup/WindowsModsScript.bat
@@ -1,25 +1,24 @@
 @rem Please UPDATE this version number ANY time changes are made to the file.
-@echo Janksters Java-Build gradlew.bat mods Version 1.0
+@echo Janksters Java-Build gradlew.bat mods Version 1.05
 
 @rem Make sure the location exists for building to be successful
-if not exist .\src\main\java\org\ (
-  mkdir .\src\main\java\org
-  mkdir .\src\main\java\org\janksters
+@if not exist .\src\main\java\org\ (
+  @mkdir .\src\main\java\org
+  @mkdir .\src\main\java\org\janksters
   @rem Copy the .gitignore file to its new home one time.
-  cp ..\JanksterNewProjectSetup\gitignoreForLinkArea .\src\main\java\.gitignore
+  @copy ..\JanksterNewProjectSetup\gitignoreForLinkArea .\src\main\java\.gitignore
 )
 
 @rem Now let's make sure the destination is writable
-attrib -R .\src\main\java\org /S
+@attrib -R .\src\main\java\org\* /S
 @rem remove both folders to allow for the possibility of deleted files from the source
-rmdir .\src\main\java\org\janksters\jankyLib /S /Q
-rmdir .\src\main\java\org\janksters\CommonClassesThisYear /S /Q
-mkdir .\src\main\java\org\janksters\jankyLib
-mkdir .\src\main\java\org\janksters\CommonClassesThisYear 
+@rmdir .\src\main\java\org\janksters\jankyLib /S /Q
+@rmdir .\src\main\java\org\janksters\CommonClassesThisYear /S /Q
+@mkdir .\src\main\java\org\janksters\jankyLib
+@mkdir .\src\main\java\org\janksters\CommonClassesThisYear
 @rem Now copy the two source areas into our newly created destinations
-xcopy ..\CommonClassesThisYear\*.* .\src\main\java\org\janksters\CommonClassesThisYear\ /S
-xcopy ..\..\jankyLib\*.* .\src\main\java\org\janksters\jankyLib\ /S
+@xcopy ..\CommonClassesThisYear\*.* .\src\main\java\org\janksters\CommonClassesThisYear\ /S
+@xcopy ..\..\jankyLib\*.* .\src\main\java\org\janksters\jankyLib\ /S
 @rem Now let's make these areas read-only to protect form accidental editing of files here.
-attrib +R .\src\main\java\org\janksters\jankyLib /S
-attrib +R .\src\main\java\org\janksters\CommonClassesThisYear /S
-
+@attrib +R .\src\main\java\org\janksters\jankyLib\* /S
+@attrib +R .\src\main\java\org\janksters\CommonClassesThisYear\* /S
