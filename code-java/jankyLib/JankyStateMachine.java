2d2fe7bcc1a34474045955e84796ed76ab37dcb6
Robert Wolff <bob.wolff68@gmail.com>
2022-02-06 15:30:58 -0800

    Bringing JankyStateMachine and associated Example into master branch

---
+++ code-java/jankyLib/JankyStateMachine.java
@@ -0,0 +1,64 @@
+
+package org.janksters.jankyLib;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public abstract class JankyStateMachine extends JankyTask{
+    private final int MAX_NAMES = 30;
+
+	private int currentState;
+	private int priorState;
+	private String machineName;
+
+    protected String[] names = new String[MAX_NAMES];
+
+    public JankyStateMachine(){
+        System.out.println("JankyStateMachine() constructor\n");
+        
+        for (int i=0; i< MAX_NAMES; i++){
+            SetName(i, "State-Default-"+i);
+        }
+        
+        machineName = "DefaultMachine";
+        currentState = 0;		// Make a sane initial choice.
+        priorState = -1;
+    }
+    
+    
+    public void SetMachineName(String machine){
+        machineName = machine;
+    }
+    public int GetCurrentState(){
+        return currentState;
+    }
+    
+    public void Run(){
+        if (priorState != currentState){
+            StateEngine(currentState, true);
+            priorState = currentState;
+        } else {
+            StateEngine(currentState, false);
+        }
+        //priorState = currentState;
+    }
+    
+    public void NewState(int newState, String reason){
+        if (newState < MAX_NAMES){
+            System.out.println("STATECHANGE: " +machineName+" OLD: "+names[currentState]+" NEW: "+names[newState]+" REASON: "+reason);
+            SmartDashboard.putString("new", names[newState]);
+            SmartDashboard.putString("old", names[currentState]);
+            currentState = newState;
+        } else {
+            System.out.println("JankyStateMachine NewState ERROR invalid newState");
+        }
+    }
+    
+    public void SetName(int state, String stateName) {
+        if (state < MAX_NAMES){   
+            names[state] = stateName;
+        } else {
+            System.out.println("JankyStateMachine SetName Bad state value/name");
+        }
+    }
+
+    protected abstract void StateEngine(int curState, boolean onStateEntered);
+}
