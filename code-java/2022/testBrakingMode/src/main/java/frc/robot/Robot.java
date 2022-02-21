c888e063a8a5e61ce85ac0d9dce7e319fe2ce432
gerrylab <gerrylab@pacbell.net>
2022-02-21 15:53:03 -0800

    Braking test complete
    Spins motor for 7 seconds then stops it and hits braking mode. Then when "Disable" is pressed on the driver station, it keeps braking mode for 5 seconds and then enters COAST mode.

--- code-java/2022/testBrakingMode/src/main/java/frc/robot/Robot.java
+++ code-java/2022/testBrakingMode/src/main/java/frc/robot/Robot.java
@@ -8,11 +8,13 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 
 import org.janksters.jankyLib.*;
 
@@ -29,7 +31,9 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private WPI_TalonFX falcon;
+  private WPI_TalonFX falcon = null;
+  private Timer teleTimer = null;
+  private Timer disTimer = null;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -41,6 +45,13 @@ public class Robot extends TimedRobot {
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
     falcon = new WPI_TalonFX(1);
+    falcon.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 30, 35, 1.0));
+//    falcon.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 20, 25, 1.0));
+//    falcon.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 2, 3, 1.0));
+//    falcon.enableCurrentLimit();
+    teleTimer = new Timer();
+    disTimer = new Timer();
+    teleTimer.reset();
   }
 
   /**
@@ -86,17 +97,21 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    teleTimer.reset();
+    teleTimer.start();
+  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    SmartDashboard.putNumber("matchtime:", DriverStation.getMatchTime());
-    if (DriverStation.getMatchTime()<7) {
+    SmartDashboard.putNumber("timer:", teleTimer.get());
+    if (teleTimer.get()<7) {
       SmartDashboard.putString("status", "Falcon Spinning");
       falcon.set(0.25);
     }
     else {
+      falcon.set(0.0);
       SmartDashboard.putString("status", "Braking Mode.");
       falcon.setNeutralMode(NeutralMode.Brake);
     }
@@ -104,13 +119,31 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    disTimer.reset();
+    disTimer.start();
+  }
 
   /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {
-    SmartDashboard.putNumber("matchtime:", DriverStation.getMatchTime());
-    SmartDashboard.putString("status", "Robot Disabled Now.");
+    SmartDashboard.putNumber("timer:", disTimer.get());
+  
+    if (teleTimer.get() > 0) {
+      if (disTimer.get() < 5) {
+        SmartDashboard.putString("status", "Robot Disabled+BRAKING.");
+        falcon.setNeutralMode(NeutralMode.Brake);
+      }
+      else {
+        SmartDashboard.putString("status", "Robot Disabled+COAST Mode.");
+        falcon.setNeutralMode(NeutralMode.Coast);
+      }
+    }
+    else {
+      SmartDashboard.putString("status", "Robot Disabled+ERROR.");
+    }
+
+
   }
 
   /** This function is called once when test mode is enabled. */
