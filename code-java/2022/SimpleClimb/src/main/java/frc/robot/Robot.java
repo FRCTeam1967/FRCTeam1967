f80d981f0e477dc7d0165c451f5e33b4396f1bfb
TiffanyN22 <tnguyen22@ndsj.org>
2022-03-10 20:38:58 -0800

    Simple climb code - commented out pneumatics, set falcon speed with joysticks, added Brake
    tested and works to lift the robot and hold a position

--- code-java/2022/SimpleClimb/src/main/java/frc/robot/Robot.java
+++ code-java/2022/SimpleClimb/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import org.janksters.jankyLib.*;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -16,25 +23,35 @@ import org.janksters.jankyLib.*;
  * project.
  */
 public class Robot extends TimedRobot {
-  private final int PCMChannel = 12;
-  private final int SolChannel = 0;
-  private final int falconChannelL = 5;
-  private final int falconChannelR = 6;
-  private Solenoid solenoid1; //= new Solenoid(PCMChannel, PneumaticsModuleType.CTREPCM, SolChannel);
+  // private final int PCMChannel = 12;
+  // private final int SolChannel = 0;
+  private final int FALCON_LEFT_CHANNEL = 6;
+  private final int FALCON_RIGHT_CHANNEL = 5;
+  private final double WINCH_FACTOR = 0.5;
+  
+  //private Solenoid solenoid1; //= new Solenoid(PCMChannel, PneumaticsModuleType.CTREPCM, SolChannel);
   //private final Solenoid solenoid2 = new Solenoid(PCMChannel, PneumaticsModuleType.CTREPCM, SolChannel);
-  private final JankyTalonFX falconL = new JankyTalonFX(falconChannelL);
-  private final JankyTalonFX falconR = new JankyTalonFX(falconChannelR);
-  /* hi hi hi hihihihihihiihihihihihihii */
+  private WPI_TalonFX falconL; //= new JankyTalonFX(falconChannelL);
+  private WPI_TalonFX falconR; //= new JankyTalonFX(falconChannelR);
   private jankyXboxJoystick XboxController = new jankyXboxJoystick(2);
-  private PneumaticsControlModule pcm;
+  //private PneumaticsControlModule pcm
+
+  private final int kTimeoutMs = 1;
+  private final int kPIDLoopIdx = 0;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    solenoid1 = new Solenoid(PCMChannel, PneumaticsModuleType.CTREPCM, SolChannel);
-    pcm = new PneumaticsControlModule(PCMChannel);
+    falconL = new WPI_TalonFX(FALCON_LEFT_CHANNEL);
+    falconR = new WPI_TalonFX(FALCON_RIGHT_CHANNEL);
+    falconL.setNeutralMode(NeutralMode.Brake);
+    falconR.setNeutralMode(NeutralMode.Brake);
+    //solenoid1 = new Solenoid(PCMChannel, PneumaticsModuleType.CTREPCM, SolChannel);
+    //pcm = new PneumaticsControlModule(PCMChannel);
+
   }
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
@@ -56,18 +73,38 @@ public class Robot extends TimedRobot {
    * chooser code above as well.
    */
   @Override
-  public void autonomousInit() {
-  }
+  public void autonomousInit() {}
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-
-  }
+  public void autonomousPeriodic() {}
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
-    falconL.setUpJankyTalonFX();
-    falconR.setUpJankyTalonFX();
+    falconL.configFactoryDefault();
+    falconL.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, kPIDLoopIdx, kTimeoutMs);
+    falconL.setInverted(TalonFXInvertType.CounterClockwise);
+    //falconL.setSensorPhase(false);
+    falconL.configNominalOutputForward(0, kTimeoutMs);
+    falconL.configNominalOutputReverse(0, kTimeoutMs);
+    falconL.configPeakOutputForward(1, kTimeoutMs);
+		falconL.configPeakOutputReverse(-1, kTimeoutMs);
+
+    falconR.configFactoryDefault();
+    falconR.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, kPIDLoopIdx, kTimeoutMs);
+    falconR.setInverted(TalonFXInvertType.Clockwise);
+    //falconR.setSensorPhase(true);
+    falconR.configNominalOutputForward(0, kTimeoutMs);
+    falconR.configNominalOutputReverse(0, kTimeoutMs);
+    falconR.configPeakOutputForward(1, kTimeoutMs);
+		falconR.configPeakOutputReverse(-1, kTimeoutMs);
+
+    falconL.getSensorCollection().setIntegratedSensorPosition(0, 10);
+    falconR.getSensorCollection().setIntegratedSensorPosition(0, 10);
+
+    //falconL.setUpJankyTalonFX();
+    //falconR.setUpJankyTalonFX();
+    // falconL.setInverted(TalonFXInvertType.CounterClockwise);
+    // falconR.setInverted(TalonFXInvertType.Clockwise);
     /* allowable error
     falconL.configAllowableClosedloopError(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
     falconR.configAllowableClosedloopError(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
@@ -81,27 +118,38 @@ public class Robot extends TimedRobot {
     falconR.config_kP(Constants.kPIDLoopIdx, Constants.winchDownKP, Constants.kTimeoutMs);
     falconR.config_kI(Constants.kPIDLoopIdx, Constants.winchDownKI, Constants.kTimeoutMs);
     falconR.config_kD(Constants.kPIDLoopIdx, Constants.winchDownKD, Constants.kTimeoutMs); */
+    
   }
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     SmartDashboard.putNumber("falconL encoder count", falconL.getSensorCollection().getIntegratedSensorPosition());
     SmartDashboard.putNumber("falconR encoder count", falconR.getSensorCollection().getIntegratedSensorPosition());
+    SmartDashboard.putNumber("falconL encoder count 2", falconL.getSelectedSensorPosition());
+    SmartDashboard.putNumber("falconR encoder count 2", falconR.getSelectedSensorPosition());
+    // SmartDashboard.putNumber("pcm solenoids", pcm.getSolenoids());
+    // SmartDashboard.putNumber("compressor current", pcm.getCompressorCurrent());
+    // SmartDashboard.putBoolean("compressor?", pcm.getCompressor());
 
-    SmartDashboard.putNumber("pcm solenoids", pcm.getSolenoids());
-    SmartDashboard.putNumber("compressor current", pcm.getCompressorCurrent());
-    SmartDashboard.putBoolean("compressor?", pcm.getCompressor());
+    SmartDashboard.putNumber("left xbox stick", XboxController.GetLeftYAxis());
+    SmartDashboard.putNumber("right xbox stick", XboxController.GetRightYAxis());
     
-    if(XboxController.GetButtonA()){
-      solenoid1.set(true);
-      //solenoid2.set(true);
-    } else if (XboxController.GetButtonY()){
-      solenoid1.set(false);
-      //solenoid2.set(false);
-    }
-    if (XboxController.GetRightYAxis() > 0.2 || XboxController.GetRightYAxis() < -0.2) {
-      falconL.set(XboxController.GetRightYAxis());
-      falconR.set(XboxController.GetRightYAxis());
+    // if(XboxController.GetButtonA()){
+    //   solenoid1.set(true);
+    //   //solenoid2.set(true);
+    // } else if (XboxController.GetButtonY()){
+    //   solenoid1.set(false);
+    //   //solenoid2.set(false);
+    // }
+    if(XboxController.GetLeftYAxis() > 0.2 ){
+      falconL.set(0.01);
+      falconR.set(0.01);
+    } else if(XboxController.GetLeftYAxis() < -0.2 ){
+      falconL.set(-0.01);
+      falconR.set(-0.01);
+    } else if (XboxController.GetRightYAxis() > 0.2 || XboxController.GetRightYAxis() < -0.2) {
+      falconL.set(XboxController.GetRightYAxis() * WINCH_FACTOR);
+      falconR.set(XboxController.GetRightYAxis() * WINCH_FACTOR);
     } else {
       falconL.set(0);
       falconR.set(0);
@@ -109,7 +157,9 @@ public class Robot extends TimedRobot {
   }
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+
+  }
   /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {}
