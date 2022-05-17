9ab643e8d5a48a8bb9424b92b4beeac2e717c055
Robert Wolff <bob.wolff68@gmail.com>
2022-05-16 22:06:20 -0700

    TEMPLATE for 2023 Java-build
    This project utilizes a critical addition in build.gradle to support both ../CommonClassesThisYear as well as ../../jankyLib
    
    Check out the `sourceSets {}` in build.gradle.
    
    Additionally, in Robot.java you'll find the usage of JankyXboxJoystick with package name/import and declaration.

---
+++ code-java/2022/testGradle/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+//import org.janksters.jankyLib.jankyXboxJoystick;
+import org.janksters.jankyLib.*;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * arcade steering.
+ */
+public class Robot extends TimedRobot {
+  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
+  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+  private final Joystick m_stick = new Joystick(0);
+  private jankyXboxJoystick xbox = null;
+
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightMotor.setInverted(true);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
+  }
+}
