4135057a116601e3f34b56280c2361944318cbec
SamanyaG <sgirish25@ndsj.org>
2023-01-16 12:05:41 -0800

    Updated JankyTask.java with overloaded methods for more consistency throughout the class

--- code-java/jankyLib/JankyTask.java
+++ code-java/jankyLib/JankyTask.java
@@ -70,6 +70,16 @@ public abstract class JankyTask extends Thread{
         System.out.println("JankyTask is exiting");
     }
 
+    public void Start(){
+        if (!baseThreadStarted){
+            super.start(); 
+            baseThreadStarted = true;
+        }
+        
+        enabled_ = true;
+        //System.out.println("Start-Enabled is " + enabled_);
+    }
+
     //start(): allows you to start or resume the task
     public void start(){
         if (!baseThreadStarted){
@@ -87,8 +97,41 @@ public abstract class JankyTask extends Thread{
         //System.out.println("Pause-Enabled is " + enabled_);
     }
 
+    public void pause(){
+        enabled_ = false;
+        //System.out.println("Pause-Enabled is " + enabled_);
+    }
+
     //Terminate(): permenantely exits the tasks (exits the while loop in run())
     public void Terminate(){
+        if (!running_){  //exits immediately if Terminate is called more than once 
+            return;
+        }
+        running_ = false;
+        //System.out.println("Terminante-Enabled is " + enabled_);
+
+        // Above told the task to exit on the next loop around.
+        // That could take 2ms or 50ms based on whether it's in pause or run and how long
+        // the actual Run() routine takes too. So we have to wait until we're really terminated here.
+        //System.out.println("Currently in Terminate()");
+        if (!Thread.currentThread().getName().equals(this.getName())){  
+            //Problem - If this code was called by Run() [same thread we're trying to kill], while(!isDead_) becomes a deadlock.
+            //this solves the problem since it checks to make sure the calling thread is not the same as the derived class's thread.
+            //If the derived class's Run() is calling Terminate(), the thread would be the derived class's thread.
+            
+            //System.out.println("Currently in Terminate if statement");
+            while (!isDead_) {
+            try {
+                    Thread.sleep(1);
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+    public void terminate(){
         if (!running_){  //exits immediately if Terminate is called more than once 
             return;
         }
