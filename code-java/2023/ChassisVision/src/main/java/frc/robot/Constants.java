143c75f2bf37e02f6aec09490febed508f938aee
Varshav1 <116127015+26vvijay@users.noreply.github.com>
2023-01-16 10:32:03 -0800

    Added current limiting
    For KitBot Chassis (TalonSRX)

--- code-java/2023/ChassisVision/src/main/java/frc/robot/Constants.java
+++ code-java/2023/ChassisVision/src/main/java/frc/robot/Constants.java
@@ -4,4 +4,10 @@ public class Constants {
     public static final int LEFT_LEADER = 1;
     public static final int RIGHT_FOLLOWER = 3;
     public static final int RIGHT_LEADER = 0;
+
+    //supply configuration limit constants
+    public static final boolean SUPPLY_CURRENT_ENABLED = true;
+    public static final int SUPPLY_CURRENT_LIMIT = 10;
+    public static final int SUPPLY_CURRENT_THRESHOLD = 15;
+    public static final double SUPPLY_CURRENT_TIME = 0.5;
 }
--- code-java/2023/ChassisVision/src/main/java/frc/robot/KitBotChassis.java
+++ code-java/2023/ChassisVision/src/main/java/frc/robot/KitBotChassis.java
@@ -2,17 +2,29 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 public class KitBotChassis {
     private static final WPI_TalonSRX leftLeader = new WPI_TalonSRX(Constants.LEFT_LEADER);
     private static final WPI_TalonSRX leftFollower = new WPI_TalonSRX(Constants.LEFT_FOLLOWER);
     private static final WPI_TalonSRX rightLeader = new WPI_TalonSRX(Constants.RIGHT_LEADER);
     private static final WPI_TalonSRX rightFollower = new WPI_TalonSRX(Constants.RIGHT_FOLLOWER);
+
     private static final MotorControllerGroup leftGroup = new MotorControllerGroup(leftFollower, leftLeader);
     private static final MotorControllerGroup rightGroup = new MotorControllerGroup(rightFollower, rightLeader);
     private static final DifferentialDrive myRobot = new DifferentialDrive(leftGroup, rightGroup);
+
     private Joystick leftJoystick, rightJoystick;
     private double averageSpeed;
+
+    public void configCurrentLimits(){
+        //StatorCurrentLimitConfiguration statorCurrentLimit = new StatorCurrentLimitConfiguration(true, 20, 15, 1.0);
+        //leftLeader.configStatorCurrentLimit(statorCurrentLimit);
+        SupplyCurrentLimitConfiguration supplyCurrentLimit = new SupplyCurrentLimitConfiguration(Constants.SUPPLY_CURRENT_ENABLED, Constants.SUPPLY_CURRENT_LIMIT, Constants.SUPPLY_CURRENT_THRESHOLD, Constants.SUPPLY_CURRENT_TIME);
+        leftLeader.configSupplyCurrentLimit(supplyCurrentLimit);
+
+    }
     public void driveTank () {
         if (leftJoystick.getRawButton(1) || rightJoystick.getRawButton(1)) {
             averageSpeed = (leftJoystick.getY() + rightJoystick.getY())/2;
