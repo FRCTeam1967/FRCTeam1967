2e37d2c1da107ff77cecfc47fe78603c927a9651
SamanyaG <sgirish25@ndsj.org>
2023-01-21 15:16:11 -0800

    Updated Intake Folder
    Updated speeds, motor ID values, removed idle timer, and cleaned up conditionals

--- code-java/2023/Intake/src/main/java/frc/robot/Robot.java
+++ code-java/2023/Intake/src/main/java/frc/robot/Robot.java
@@ -20,6 +20,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  Intake intake = new Intake();
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
