5dd1f9a574242c3403a5e3d979596583d0f260fa
Michael Santos <drsandytoes@gmail.com>
2023-02-25 21:12:51 -0800

    Commit project as it was when used for Robot Showcase demos

--- code-java/2023/Examples/TextExample/src/main/java/frc/robot/RobotContainer.java
+++ code-java/2023/Examples/TextExample/src/main/java/frc/robot/RobotContainer.java
@@ -149,8 +149,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null; // Autos.exampleAuto(m_exampleSubsystem);
+    return fontTestCommand();
   }
 
   public void initEncoder() {
